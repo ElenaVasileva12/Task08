92d574939993fc994a48d199909f2c6ba57cf1fa
Admin <elenaver1984@ya.ru>
2023-05-23 10:29:40 +0500

    Task08

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int n=Convert.ToInt32(Console.ReadLine());
+int i=0;
+
+if (n>0)
+{
+    while (i<(n-2))
+    {
+        i=i+2;
+        Console.Write($"{i}, ");
+    } 
+}
+else Console.WriteLine($"{n} должно быть > 0  ");
